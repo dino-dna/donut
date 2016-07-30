946a34f99abbf90a63e44385a83268528e7dd609
Cory Reed <creed@mrn.org>
2016-07-29 20:50:26 -0700

    Add some donut comp stylin' :doughnut:

--- src/components/Donut.js
+++ src/components/Donut.js
@@ -3,6 +3,13 @@ import '../css/Donut.css'
 
 export default class Donut extends Component {
   render() {
-    return <div className="Donut">Imma donut!!!</div>
+    return (
+      <div className="Donut">
+        <svg viewBox="0 0 100 100">
+          <circle cx={50} cy={50} r={50} fill="#efcc9a" stroke="#20426a" stroke-width="10" />
+          <circle cx={50} cy={50} r={10} fill="white" stroke="#20426a" stroke-width="10" />
+        </svg>
+      </div>
+    );
   }
 }
