84728085fe1a47df16e29a675537522d596c9620
Cory Reed <creed@mrn.org>
2016-07-29 21:34:34 -0700

    Add some settings form markup.

--- src/components/DonutSettings.js
+++ src/components/DonutSettings.js
@@ -3,7 +3,79 @@ import React, { Component } from 'react';
 class DonutSettings extends Component {
   render() {
     const { donut } = this.props;
-    return <pre>{donut ? JSON.stringify(donut, null, 2) : 'no donut :('}</pre>
+
+    const DONUT_FROSTING_COVERAGE = 0;
+    const DONUT_FROSTING_THICKNESS = 0;
+    const DONUT_SPRINKLE_COVERAGE = 0;
+    const DONUT_INNER_RADIUS = 0;
+    const DONUT_OUTER_RADIUS = 0;
+
+    const wat = function() {}
+
+    onChangeFrostingCoverage = wat;
+    onChangeFrostingThickness = wat;
+    onChangeSprinkleCoverage = wat;
+    onChangeInnerRadius = wat;
+    onChangeOuterRadius = wat;
+
+    return (
+      <form>
+        <h2>Make your donut:</h2>
+        <pre>{donut ? JSON.stringify(donut, null, 2) : 'no donut :('}</pre>
+
+        <div className="DonutSettings-control">
+          <label>Frosting Coverage:</label>
+          <input
+            onChange={onChangeFrostingCoverage}
+            type="range"
+            min="0"
+            max="1"
+            value={DONUT_FROSTING_COVERAGE}
+          />
+        </div>
+        <div className="DonutSettings-control">
+          <label>Frosting Thickness:</label>
+          <input
+            onChange={onChangeFrostingThickness}
+            type="range"
+            min="0"
+            max="1"
+            value={DONUT_FROSTING_THICKNESS}
+          />
+        </div>
+        <div className="DonutSettings-control">
+          <label>Sprinkle Coverage:</label>
+          <input
+            onChange={onChangeSprinkleCoverage}
+            type="range"
+            min="0"
+            max="1"
+            value={DONUT_SPRINKLE_COVERAGE}
+          />
+        </div>
+        <div className="DonutSettings-control">
+          <label>Inner Radius:</label>
+          <input
+            onChange={onChangeInnerRadius}
+            type="range"
+            min="0"
+            max="1"
+            value={DONUT_INNER_RADIUS}
+          />
+        </div>
+        <div className="DonutSettings-control">
+          <label>Outer Radius:</label>
+          <input
+            onChange={onChangeOuterRadius}
+            type="range"
+            min="0"
+            max="1"
+            value={DONUT_OUTER_RADIUS}
+          />
+        </div>
+
+      </form>
+    );
   }
 }
 
