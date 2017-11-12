fe95b2789d173d43069d186be4eec5e1299e4a1f
Cory Reed <swashcap@gmail.com>
2017-11-12 12:30:49 -0800

    feat(monster): add sample uploader script

---
+++ packages/donut-monster/scripts/sampler.js
@@ -0,0 +1,30 @@
+/**
+ * Sample donut creator.
+ *
+ * Push up sample donuts to the donut monster. Useful for previewing UIs.
+ */
+
+const io = require('socket.io-client')
+const { getRandomDonuts, messages } = require('donut-common')
+
+const socket = io('http://localhost:3001')
+const uploadDonuts = () => {
+  const randos = getRandomDonuts(Math.ceil(Math.random() * 20))
+  console.log(`Uploading ${randos.length} random donuts`)
+  socket.emit(messages.UPLOAD_DONUTS, randos)
+}
+
+socket.on('connect', () => {
+  console.log('Connected')
+})
+socket.on('disconnect', (reason) => {
+  console.log(`Disconnected: ${reason}`)
+})
+socket.on('error', error => {
+  throw error
+})
+
+socket.on(messages.INIT_CLIENT, () => {
+  uploadDonuts()
+  setInterval(uploadDonuts, 5000)
+})
