101c391e220cc2d094196c8ce7678eda15c54c0a
Cory Reed <swashcap@gmail.com>
2017-11-26 12:24:46 -0800

    feat(ui): debounce donut form sliders

--- packages/ui/src/containers/Create.js
+++ packages/ui/src/containers/Create.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import debounce from 'lodash/debounce'
 import Donut from '../components/Donut'
 import DonutForm from '../components/DonutForm'
 import '../css/Create.css'
@@ -14,28 +15,52 @@ import {
 } from '../state/ducks/donut'
 import { addDonut } from '../state/ducks/donuts'
 
-const Create = ({
-  addDonut,
-  donut,
-  onChangeFrostingCoverage,
-  onChangeFrostingThickness,
-  onChangeInnerRadius,
-  onChangeOuterRadius,
-  onChangeSprinkleCoverage
-}) => (
-  <div className='Create'>
-    <Donut {...donut} />
-    <DonutForm
-      donut={donut}
-      onChangeFrostingCoverage={onChangeFrostingCoverage}
-      onChangeFrostingThickness={onChangeFrostingThickness}
-      onChangeInnerRadius={onChangeInnerRadius}
-      onChangeOuterRadius={onChangeOuterRadius}
-      onChangeSprinkleCoverage={onChangeSprinkleCoverage}
-      onSave={addDonut}
-    />
-  </div>
-)
+class Create extends Component {
+  constructor (props, ...args) {
+    super(props, ...args)
+
+    const getDebounced = attribute => debounce(
+      value => props.setDonutAttribute({
+        attribute,
+        value
+      }),
+      100,
+      {
+        leading: true,
+        trailing: false,
+        maxWait: 100
+      }
+    )
+
+    this.handleFrostingCoverageChange = getDebounced(DONUT_FROSTING_COVERAGE)
+    this.handleFrostingThicknessChange = getDebounced(DONUT_FROSTING_THICKNESS)
+    this.handleInnerRadiusChange = getDebounced(DONUT_INNER_RADIUS)
+    this.handleOuterRadiusChange = getDebounced(DONUT_OUTER_RADIUS)
+    this.handleSprinkleCoverageChange = getDebounced(DONUT_SPRINKLE_COVERAGE)
+  }
+
+  render () {
+    const {
+      addDonut,
+      donut
+    } = this.props
+
+    return (
+      <div className='Create'>
+        <Donut {...donut} />
+        <DonutForm
+          donut={donut}
+          onChangeFrostingCoverage={this.handleFrostingCoverageChange}
+          onChangeFrostingThickness={this.handleFrostingThicknessChange}
+          onChangeInnerRadius={this.handleInnerRadiusChange}
+          onChangeOuterRadius={this.handleOuterRadiusChange}
+          onChangeSprinkleCoverage={this.handleSprinkleCoverageChange}
+          onSave={addDonut}
+        />
+      </div>
+    )
+  }
+}
 
 Create.propTypes = {
   addDonut: PropTypes.func.isRequired,
@@ -46,26 +71,13 @@ Create.propTypes = {
     DONUT_INNER_RADIUS: PropTypes.number.isRequired,
     DONUT_OUTER_RADIUS: PropTypes.number.isRequired
   }).isRequired,
-  onChangeFrostingCoverage: PropTypes.func.isRequired,
-  onChangeFrostingThickness: PropTypes.func.isRequired,
-  onChangeInnerRadius: PropTypes.func.isRequired,
-  onChangeOuterRadius: PropTypes.func.isRequired,
-  onChangeSprinkleCoverage: PropTypes.func.isRequired
+  setDonutAttribute: PropTypes.func.isRequired
 }
-
-const getAttributeSetter = attribute => value => setDonutAttribute({
-  attribute,
-  value
-})
 
 export default connect(
   ({ donut }) => ({ donut }),
   {
     addDonut,
-    onChangeFrostingCoverage: getAttributeSetter(DONUT_FROSTING_COVERAGE),
-    onChangeFrostingThickness: getAttributeSetter(DONUT_FROSTING_THICKNESS),
-    onChangeSprinkleCoverage: getAttributeSetter(DONUT_SPRINKLE_COVERAGE),
-    onChangeInnerRadius: getAttributeSetter(DONUT_INNER_RADIUS),
-    onChangeOuterRadius: getAttributeSetter(DONUT_OUTER_RADIUS)
+    setDonutAttribute
   }
 )(Create)
