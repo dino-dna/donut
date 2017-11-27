ed6f557d9d9ab40e7c4e7e8bcae5d55583c7041e
Cory Reed <swashcap@gmail.com>
2017-11-26 17:38:18 -0800

    refactor(ui): move ratings into prediction viewer table

---
+++ packages/ui/src/components/RatingWithScores.js
@@ -0,0 +1,118 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import round from 'lodash/round'
+
+import { getIndicator, getIndicatorParams } from 'donut-common/src/rater'
+
+import RatingIndicator from './RatingIndicator'
+
+const RatingWithScores = ({
+  DONUT_FROSTING_COVERAGE,
+  DONUT_FROSTING_THICKNESS,
+  DONUT_INNER_RADIUS,
+  DONUT_OUTER_RADIUS,
+  DONUT_SPRINKLE_COVERAGE,
+  error
+}) => {
+  const {
+    frostingCoverage,
+    frostingThickness,
+    radius,
+    sprinkleCoverage
+  } = getIndicatorParams({
+    DONUT_FROSTING_COVERAGE,
+    DONUT_FROSTING_THICKNESS,
+    DONUT_INNER_RADIUS,
+    DONUT_OUTER_RADIUS,
+    DONUT_SPRINKLE_COVERAGE
+  })
+  const score = getIndicator({
+    DONUT_FROSTING_COVERAGE,
+    DONUT_FROSTING_THICKNESS,
+    DONUT_INNER_RADIUS,
+    DONUT_OUTER_RADIUS,
+    DONUT_SPRINKLE_COVERAGE
+  })
+
+  return (
+    <div className='Rating RatingWithScores'>
+      <table>
+        <thead>
+          <tr>
+            <th />
+            <th>Values</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope='row'>Frosting coverage:</th>
+            <td>
+              <code>{round(DONUT_FROSTING_COVERAGE, 4).toFixed(4)}</code>
+            </td>
+            <td>
+              <RatingIndicator rating={frostingCoverage} />
+            </td>
+          </tr>
+          <tr>
+            <th scope='row'>Frosting thickness:</th>
+            <td>
+              <code>{round(DONUT_FROSTING_THICKNESS, 4).toFixed(4)}</code>
+            </td>
+            <td>
+              <RatingIndicator rating={frostingThickness} />
+            </td>
+          </tr>
+          <tr>
+            <th scope='row'>Radius:</th>
+            <td>
+              <code>{round(DONUT_INNER_RADIUS, 4).toFixed(4)}</code>,
+              <code>{round(DONUT_OUTER_RADIUS, 4).toFixed(4)}</code>
+            </td>
+            <td>
+              <RatingIndicator rating={radius} />
+            </td>
+          </tr>
+          <tr>
+            <th scope='row'>Sprinkles:</th>
+            <td>
+              <code>{round(DONUT_SPRINKLE_COVERAGE, 4).toFixed(4)}</code>
+            </td>
+            <td>
+              <RatingIndicator rating={sprinkleCoverage} />
+            </td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <th scope='row'>Overall</th>
+            <td>
+              <code>{round(score, 4).toFixed(4)}</code>
+            </td>
+            <td>
+              <RatingIndicator rating={score} />
+            </td>
+          </tr>
+          <tr>
+            <th scope='row'>Error</th>
+            <td>
+              <code style={{ color: 'red' }}>{round(error, 4).toFixed(4)}</code>
+            </td>
+            <td />
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  )
+}
+
+RatingWithScores.propTypes = {
+  DONUT_FROSTING_COVERAGE: PropTypes.number.isRequired,
+  DONUT_FROSTING_THICKNESS: PropTypes.number.isRequired,
+  DONUT_INNER_RADIUS: PropTypes.number.isRequired,
+  DONUT_OUTER_RADIUS: PropTypes.number.isRequired,
+  DONUT_SPRINKLE_COVERAGE: PropTypes.number.isRequired,
+  error: PropTypes.number.isRequired
+}
+
+export default RatingWithScores
