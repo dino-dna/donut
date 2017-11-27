327c032e0f22a5ea9e2987401ad427493d10b44d
Cory Reed <swashcap@gmail.com>
2017-11-26 17:38:08 -0800

    fix(ui): correct bad prop-type validation

--- packages/ui/src/components/DonutViewer.js
+++ packages/ui/src/components/DonutViewer.js
@@ -60,18 +60,21 @@ class DonutViewer extends Component {
   }
 }
 
+DonutViewer.defaultProps = {
+  donuts: []
+}
+
 DonutViewer.propTypes = {
-  donuts: PropTypes.arrayOf(PropTypes.oneOfType([
+  donuts: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.shape({
       DONUT_FROSTING_COVERAGE: PropTypes.number.isRequired,
       DONUT_FROSTING_THICKNESS: PropTypes.number.isRequired,
       DONUT_INNER_RADIUS: PropTypes.number.isRequired,
       DONUT_OUTER_RADIUS: PropTypes.number.isRequired,
-      DONUT_RATING: PropTypes.number.isRequired,
       DONUT_SPRINKLE_COVERAGE: PropTypes.number.isRequired
     })
-  ]).isRequired)
+  ])))
 }
 
 export default DonutViewer
