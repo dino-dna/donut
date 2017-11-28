cb708160c39135a8fe4ff1a8280e58c1a2869138
Cory Reed <swashcap@gmail.com>
2017-11-28 01:14:53 -0800

    refactor(ui): make HACKS a getter/setter

--- packages/ui/src/middleware/admin.js
+++ packages/ui/src/middleware/admin.js
@@ -8,14 +8,19 @@ export default ({ dispatch, getState }) => next => action => {
   // This is a **hack** to expose admin functionality through the console
   // TODO: Unhack it! Make a button or something.
   if (typeof window && !('__HACKS__' in window)) {
-    window.__HACKS__ = () => {
-      const { admin: { isAdmin } } = getState()
-      if (isAdmin) {
-        dispatch(disableAdminMode())
-      } else {
-        dispatch(enableAdminMode())
+    Object.defineProperty(window, '__HACKS__', {
+      get () {
+        const { admin: { isAdmin } } = getState()
+        if (isAdmin) {
+          dispatch(disableAdminMode())
+        } else {
+          dispatch(enableAdminMode())
+        }
+      },
+      set (value) {
+        dispatch(value ? enableAdminMode() : disableAdminMode())
       }
-    }
+    })
   }
 
   return next(action)
